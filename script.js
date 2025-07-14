3de8def0342d496c544383564ca66cef32beb325
verticetv <noreply_01@mail.ru>
2025-07-14 17:57:06 -0400

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("arrow").addEventListener("click", function() {
-    const options = document.getElementById("download-options");
-    options.style.display = options.style.display === "flex" ? "none" : "flex";
-});
