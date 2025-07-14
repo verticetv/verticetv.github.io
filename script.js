5e0cc8ec1a7d7a91fc5a9718752b4c142aae8771
verticetv <noreply_01@mail.ru>
2025-07-14 17:26:31 -0400

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// script.js
-document.getElementById('toggleButtons').addEventListener('click', () => {
-  document.getElementById('platformButtons').classList.toggle('hidden');
+document.getElementById("arrow").addEventListener("click", function() {
+    const options = document.getElementById("download-options");
+    options.style.display = options.style.display === "flex" ? "none" : "flex";
 });
